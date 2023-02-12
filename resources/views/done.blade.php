f1aa8ec2ab64571c5ad87a7f2fe683b3f070f537
hallykoppter <officialkurniasandi@gmail.com>
2023-02-12 16:15:36 +0700

    up

--- resources/views/done.blade.php
+++ resources/views/done.blade.php
@@ -1,4 +1,4 @@
-{{ header('refresh: 3; url:/logout') }}
+{{header("Refresh:10; url=/logout");}}
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +8,70 @@
   <title>OSIS | e-Lection</title>
   <link rel="stylesheet" href="/css/main_style.css">
   <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+  {{-- Progress Bar --}}
+  <style>
+    .progress2 {
+      border-radius: 30px;
+      background-color: #fff;
+      width: 70vw;
+    }
+
+    .progress-bar2 {
+      height: 18px;
+      border-radius: 30px;
+      transition: 0.4s linear;
+      transition-property: width, background-color;
+    }
+
+    .progress-moved .progress-bar2 {
+      background-color: #f3c623;
+      animation: progress 10s;
+    }
+
+    @keyframes progress {
+      0% {
+        width: 0%;
+        background: #f9bcca;
+      }
+
+      100% {
+        width: 100%;
+        background: #f3c623;
+        box-shadow: 0 0 40px #f3c623;
+      }
+    }
+
+    .icon {
+      color: #f3c623;
+      animation: icon 10s infinite;
+      background-color: transparent;
+      padding-right: 400px;
+      padding-bottom: 20px;
+    }
+
+    @keyframes icon {
+      0% {
+        opacity: 0.2;
+        text-shadow: 0 0 0 #f3c623;
+      }
+
+      100% {
+        opacity: 1;
+        text-shadow: 0 0 10px #f3c623;
+      }
+    }
+
+
+    @keyframes p {
+
+      90%,
+      100% {
+        --p: 100;
+      }
+    }
+
+  </style>
 </head>
 <body>
 
@@ -30,9 +94,15 @@
         </div>
         <div class="row">
             <div class="col-lg text-center">
-                Halaman ini akan otomatis keluar dalam ..
+                Halaman ini akan keluar otomatis
             </div>
         </div>
+
+        {{-- Progress Bar --}}
+        <div class="progress2 mx-auto progress-moved mt-5">
+          <div class="progress-bar2"></div>
+         </div>
+        {{-- End Progress Bar --}}
 
   <script src="js/bootstrap.bundle.min.js"></script>
 </body>
