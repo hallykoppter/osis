32af4c2f6c48d4befee8a036c19627a6bc5cef27
hallykoppter <officialkurniasandi@gmail.com>
2023-02-16 12:26:02 +0700

    up

--- app/Http/Controllers/CalonController.php
+++ app/Http/Controllers/CalonController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Calon;
-use App\Http\Requests\StoreCalonRequest;
-use App\Http\Requests\UpdateCalonRequest;
 use Illuminate\Http\Request;
-use Illuminate\Http\file;
 
 class CalonController extends Controller
 {
@@ -17,7 +14,72 @@ class CalonController extends Controller
      */
     public function index()
     {
-
+        //
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Calon  $calon
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Calon $calon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Calon  $calon
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Calon $calon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Calon  $calon
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Calon $calon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Calon  $calon
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Calon $calon)
+    {
+        //
+    }
 }
